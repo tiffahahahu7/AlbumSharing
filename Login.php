92cc56c95156ffba265f22fbd99472ef5dee51de
TinTaguan <tagu0009@algonquinlive.com>
2024-11-29 23:49:09 -0500

    implement auditLogs for album deletion via database trigger

--- Login.php
+++ Login.php
@@ -38,7 +38,7 @@
                 
                 if ($user->getIsAdmin())
                 {
-                    header("Location: AdminPage.php");
+                    header("Location: Admin.php");
                     exit();
                 }
                 else
